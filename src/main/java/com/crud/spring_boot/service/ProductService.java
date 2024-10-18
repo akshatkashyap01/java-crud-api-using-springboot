9b3a952aefb06601c93ba8f9cddad65d5484832d
akshatkashyap01 <akshatkashyap01@gmail.com>
2024-10-18 17:44:05 +0530

    added service methods

--- src/main/java/com/crud/spring_boot/service/ProductService.java
+++ src/main/java/com/crud/spring_boot/service/ProductService.java
@@ -1,4 +1,35 @@
 package com.crud.spring_boot.service;
 
+import com.crud.spring_boot.model.Product;
+import com.crud.spring_boot.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    public List<Product> findByName(String name) {
+        return productRepository.findByName(name);
+    }
 }
